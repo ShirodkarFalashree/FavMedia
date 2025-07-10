57a932fd61191f50ebaa465e16cb25accec1ea5d
Avishkar68 <kakadeavishkar84@gmail.com>
2025-07-10 19:00:38 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { createBrowserRouter, RouterProvider, Outlet, Navigate } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import About from './pages/About';
+import Blogs from './pages/Blogs';
+import Templates from './pages/Templates';
+import Contact from './pages/Contact';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const Layout = () => {
   return (
     <>
-
+      <Navbar />
+      <Outlet />
+      <Footer />
     </>
-  )
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/services',
+        element: <Navigate to="/#services" replace />, // Redirect to home with hash
+      },
+      {
+        path: '/about-us',
+        element: <About />,
+      },
+      {
+        path: '/blogs',
+        element: <Blogs />,
+      },
+      {
+        path: '/templates',
+        element: <Templates />,
+      },
+      {
+        path: '/contact',
+        element: <Contact />,
+      },
+    ],
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
